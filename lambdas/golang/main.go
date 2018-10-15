6b1be15eafc19a2673edb6727125cbe66cda8c65
Carlos Roman <carlos@cliche-corp.co.uk>
2018-10-15 21:45:13 +0100

    :sparkles: Golang lambda now reads correctly from DynamoDB

--- lambdas/golang/main.go
+++ lambdas/golang/main.go
@@ -1,21 +1,54 @@
 package golang
 
 import (
-	"errors"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 	"log"
+	"net/http"
 	"os"
 )
 
 var (
-	// ErrNameNotProvided is thrown when a name is not provided
-	ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
-	db                 = dynamodb.New(session.New(), aws.NewConfig().WithRegion(os.Getenv("REGION")))
+	db = getDynamodbClient()
 )
+
+type Game struct {
+	Date                   string `json:"Date"`     // Match Date (dd/mm/yy)
+	HomeTeam               string `json:"HomeTeam"` // Home Team
+	AwayTeam               string `json:"AwayTeam"` // Away Team
+	FullTimeHomeTeamGoals  int    `json:"FTHG"`     // Full Time Home Team Goals
+	FullTimeAwayTeamGoals  int    `json:"FTAG"`     // Full Time Away Team Goals
+	FullTimeResult         string `json:"FTR"`      // Full Time Result (H=Home Win, D=Draw, A=Away Win)
+	HalfTimeHomeTeamGoals  int    `json:"HTHG"`     // Half Time Home Team Goals
+	HalfTimeAwayTeamGoals  int    `json:"HTAG"`     // Half Time Away Team Goals
+	HalfTimeResult         string `json:"HTR"`      // Half Time Result (H=Home Win, D=Draw, A=Away Win)
+	Referee                string `json:"Referee"`  // Match Referee
+	HomeTeamShots          int    `json:"HS"`       // Home Team Shots
+	AwayTeamShots          int    `json:"AS"`       // Away Team Shots
+	HomeTeamShotsOnTarget  int    `json:"HST"`      // Home Team Shots on Target
+	AwayTeamShotsOnTarget  int    `json:"AST"`      // Away Team Shots on Target
+	HomeTeamFoulsCommitted int    `json:"HF"`       // Home Team Fouls Committed
+	AwayTeamFoulsCommitted int    `json:"AF"`       // Away Team Fouls Committed
+	HomeTeamCorners        int    `json:"HC"`       // Home Team Corners
+	AwayTeamCorners        int    `json:"AC"`       // Away Team Corners
+	HomeTeamYellowCards    int    `json:"HY"`       // Home Team Yellow Cards
+	AwayTeamYellowCards    int    `json:"AY"`       // Away Team Yellow Cards
+	HomeTeamRedCards       int    `json:"HR"`       // Home Team Red Cards
+	AwayTeamRedCards       int    `json:"AR"`       // Away Team Red Cards
+}
+
+func getDynamodbClient() dynamodbiface.DynamoDBAPI {
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("REGION"))},
+	)
+	return dynamodb.New(sess)
+}
 
 // Handler is your Lambda function handler
 // It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
@@ -24,22 +57,63 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	// stdout and stderr are sent to AWS CloudWatch Logs
 	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
+	ht, ok := request.QueryStringParameters["HomeTeam"]
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			Body:       "Missing param \"HomeTeam\"",
+			StatusCode: http.StatusBadRequest,
+		}, nil
+	}
+
+	at, ok := request.QueryStringParameters["AwayTeam"]
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			Body:       "Missing param \"AwayTeam\"",
+			StatusCode: http.StatusBadRequest,
+		}, nil
+	}
+
 	input := &dynamodb.GetItemInput{
 		TableName: aws.String(os.Getenv("TABLE")),
-		Key:       map[string]*dynamodb.AttributeValue{},
+		Key: map[string]*dynamodb.AttributeValue{
+			"HomeTeam": {
+				S: aws.String(ht),
+			},
+			"AwayTeam": {
+				S: aws.String(at),
+			},
+		},
 	}
-	log.Printf("Input: %s\n", input)
 
-	// If no name is provided in the HTTP request body, throw an error
-	if len(request.Body) < 1 {
-		return events.APIGatewayProxyResponse{}, ErrNameNotProvided
+	res, err := db.GetItem(input)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Not Found",
+			StatusCode: http.StatusNotFound,
+		}, nil
+	}
+
+	g := new(Game)
+	if dynamodbattribute.UnmarshalMap(res.Item, &g) != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Internal Server Error",
+			StatusCode: http.StatusInternalServerError,
+		}, nil
+	}
+
+	js, err := json.Marshal(g)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "Internal Server Error",
+			StatusCode: http.StatusInternalServerError,
+		}, nil
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       "Hello " + request.Body,
-		StatusCode: 200,
+		Body:       string(js),
+		StatusCode: http.StatusOK,
 	}, nil
-
 }
 
 func main() {
